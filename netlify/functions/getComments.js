8f465960dd4a2107bc0b9f32770613a63ff51804
Agamendon <gosha20052811@gmail.com>
2023-07-08 18:02:53 -0400

    nodejs

--- netlify/functions/getComments.js
+++ netlify/functions/getComments.js
@@ -1,3 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+
+function getJWTNow() {
+    var private_key = fs.readFileSync('static/private.key');
+    var payload = {
+        iat: Date.now() / 1000 - 60,
+        exp: Date.now() / 1000 + 10 * 60,
+        iss: fs.readFileSync("static/appid.txt")
+    };
+}
+
+
 function hello() {
     return "Hello";
 }
